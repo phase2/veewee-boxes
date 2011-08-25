4105806658a5cb0db800a32fd51e729864cc30cd
Steven Merrill <steven.merrill@gmail.com>
2011-08-25 19:41:24 +0100

    CentOS 6 annoys me.

--- definitions/CentOS-6.0-x86_64-netboot/definition.rb
+++ definitions/CentOS-6.0-x86_64-netboot/definition.rb
@@ -6,7 +6,7 @@ Veewee::Session.declare({
   :iso_src => "http://be.mirror.eurid.eu/centos/6.0/isos/x86_64/CentOS-6.0-x86_64-netinstall.iso",
   :iso_md5 => "d13da95c29e585ee15cf403b89468243",
   :iso_download_timeout => 1000,
-  :boot_wait => "15", :boot_cmd_sequence => [ '<Tab> text ks=http://%IP%:%PORT%/ks.cfg<Enter>' ],
+  :boot_wait => "10", :boot_cmd_sequence => [ '<Tab> text ks=http://%IP%:%PORT%/ks.cfg<Enter>' ],
   :kickstart_port => "7122", :kickstart_timeout => 10000, :kickstart_file => "ks.cfg",
   :ssh_login_timeout => "100", :ssh_user => "vagrant", :ssh_password => "vagrant", :ssh_key => "",
   :ssh_host_port => "7222", :ssh_guest_port => "22",
--- definitions/CentOS-6.0-x86_64-netboot/postinstall.sh
+++ definitions/CentOS-6.0-x86_64-netboot/postinstall.sh
@@ -2,10 +2,12 @@
 
 date > /etc/vagrant_box_build_time
 
-yum -y erase wireless-tools gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
-yum -y clean all
+#yum -y erase wireless-tools gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+#yum -y clean all
 
-gem install --no-ri --no-rdoc chef
+# Sorry, Chef.
+#gem install --no-ri --no-rdoc chef
+gem install --no-ri --no-rdoc puppet
 
 # Installing vagrant keys
 mkdir /home/vagrant/.ssh
@@ -17,14 +19,26 @@ chown -R vagrant /home/vagrant/.ssh
 # Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
-wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+wget http://192.168.215.20/centos/VBoxGuestAdditions_$VBOX_VERSION.iso
+#wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
 mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm VBoxGuestAdditions_$VBOX_VERSION.iso
+shred --remove --zero --iterations=1 VBoxGuestAdditions_$VBOX_VERSION.iso
+#rm VBoxGuestAdditions_$VBOX_VERSION.iso
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
-dd if=/dev/zero of=/tmp/clean || rm /tmp/clean
+# Kill us some udev crap.
+ln -sf /dev/null /lib/udev/rules.d/75-persistent-net-generator.rules
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+
+# On startup, remove HWADDR from the eth0 interface.
+echo >> /etc/rc.local
+echo 'cp -f /etc/sysconfig/network-scripts/ifcfg-eth0 /tmp/eth0' >> /etc/rc.local
+echo 'sed "/^HWADDR/d" /tmp/eth0 > /etc/sysconfig/network-scripts/ifcfg-eth0' >> /etc/rc.local
+
+# Aha - added shredding.
+# dd if=/dev/zero of=/tmp/clean || rm /tmp/clean
 
 exit
