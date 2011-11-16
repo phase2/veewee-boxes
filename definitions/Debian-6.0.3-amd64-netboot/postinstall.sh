07f23f97d10fbbc53ff62b32195350d790858142
Steven Merrill <steven.merrill@gmail.com>
2011-11-16 17:23:52 -0500

    Move VBox additions to local address.

--- definitions/Debian-6.0.3-amd64-netboot/postinstall.sh
+++ definitions/Debian-6.0.3-amd64-netboot/postinstall.sh
@@ -42,7 +42,7 @@ aptitude -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-
 #Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
-wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+wget http://192.168.1.200/VBoxGuestAdditions_$VBOX_VERSION.iso
 #wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
 mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 yes|sh /mnt/VBoxLinuxAdditions.run
