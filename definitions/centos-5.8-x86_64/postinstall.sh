b712c06b2ca0d6e1f38a6e381248a470251eb87d
Jed Prentice <jed@phase2technology.com>
2013-03-27 15:29:20 -0400

    Updated from the template provided by 'vagrant basebox templates', plus added an argument to virtualbox to explicityly use nat dns, which is required for DNS resolution on Ubuntu 12.10 hosts and hopefully won't break anything on others

--- definitions/centos-5.8-x86_64/postinstall.sh
+++ definitions/centos-5.8-x86_64/postinstall.sh
@@ -2,34 +2,47 @@
 
 date > /etc/vagrant_box_build_time
 
-fail() {
+fail()
+{
   echo "FATAL: $*"
   exit 1
 }
 
-# kernel source is needed for vbox additions
-yum -y install kernel-devel-`uname -r`
+#kernel source is needed for vbox additions
+yum -y install gcc bzip2 make kernel-devel-`uname -r`
+#yum -y update
+#yum -y upgrade
+
+yum -y install gcc-c++ zlib-devel openssl-devel readline-devel sqlite3-devel
+yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
 yum -y clean all
 
-# Install ruby
-wget http://rubyforge.org/frs/download.php/71096/ruby-enterprise-1.8.7-2010.02.tar.gz
-tar xzvf ruby-enterprise-1.8.7-2010.02.tar.gz
-./ruby-enterprise-1.8.7-2010.02/installer -a /opt/ruby --no-dev-docs --dont-install-useful-gems
-echo 'PATH=$PATH:/opt/ruby/bin'> /etc/profile.d/rubyenterprise.sh
+#Installing ruby
+cd /tmp
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz || fail "Could not download Ruby source"
+tar xzvf ruby-1.9.2-p180.tar.gz 
+cd ruby-1.9.2-p180
+./configure
+make && make install
+cd /tmp
+rm -rf /tmp/ruby-1.9.2-p180
+rm /tmp/ruby-1.9.2-p180.tar.gz
+ln -s /usr/local/bin/ruby /usr/bin/ruby # Create a sym link for the same path
+ln -s /usr/local/bin/gem /usr/bin/gem # Create a sym link for the same path
 
-# Install chef & Puppet
-echo " Install chef and puppet"
-/opt/ruby/bin/gem install chef --no-ri --no-rdoc || fail "Could not install chef"
-/opt/ruby/bin/gem install puppet --no-ri --no-rdoc || fail "Could not install puppet"
+#Installing chef & Puppet
+echo "Installing chef and puppet"
+/usr/local/bin/gem install chef --no-ri --no-rdoc || fail "Could not install chef"
+/usr/local/bin/gem install puppet --no-ri --no-rdoc || fail "Could not install puppet"
 
-# Install vagrant keys
+#Installing vagrant keys
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
-# Install the virtualbox guest additions
+#Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
 wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
@@ -39,17 +52,9 @@ umount /mnt
 
 rm VBoxGuestAdditions_$VBOX_VERSION.iso
 
-# On startup, remove HWADDR from the eth0 interface.
-cp -f /etc/sysconfig/network-scripts/ifcfg-eth0 /tmp/eth0
-sed "/^HWADDR/d" /tmp/eth0 > /etc/sysconfig/network-scripts/ifcfg-eth0
-sed -e "s/dhcp/none/;s/eth0/eth1/" /etc/sysconfig/network-scripts/ifcfg-eth0 > /etc/sysconfig/network-scripts/ifcfg-eth1
-
-# Add kernel parameters to prevent excessive CPU stealing under virtualization.
-sed -e's/\tkernel .*/& notsc divider=10/' /boot/grub/grub.conf > /tmp/grub.conf
-mv /tmp/grub.conf /boot/grub/grub.conf
-
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 sed -i "s/^\(.*env_keep = \"\)/\1PATH /" /etc/sudoers
 
-exit
+#poweroff -h
 
+exit
